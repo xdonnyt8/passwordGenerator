fad9bef865546b42791caf9c297a720bf9dd2c5a
Don Tran <mr.dontran@gmail.com>
2020-06-06 15:01:12 -0400

    added check box parameters to generator but not getting the function to work

--- script.js
+++ script.js
@@ -2,47 +2,46 @@
 var generateBtn = document.querySelector("#generate");
 var passwordEl = document.querySelector("#passaword");
 
+var abc = document.getElementById("CaseSensitive").checked;
+var nums = document.getElementById("numbers").checked;
+var spec = document.getElementById("specChar").checked;
+
+// variables that the password would be
 var numbers = "1234567890"
-var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+// var lowerCase = "abcdefghijklmnopqrstuvwxyz"
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var specialChar = "~!@#$%^&*()_+`<>?.,/[]{}"
 
-function generatePassword()
-{
-
-var caseSense = confirm("Do you want your password to be Case-Sensitive?")
-var specialSense = confirm("Do you want special charaters?")
-var passLength = prompt("How long do you want your password?")
-
-
-if (caseSense === true && specialSense === true) {
-  
-
-
-  
-} else { 
-  if (caseSense === false && specialSense === false)
-  
-
-} else { 
-  if (condition) {
-  
-} else {
-  
+function generatePassword() {
+  var passCode = "abcdefghijklmnopqrstuvwxyz";
+  //Check if the boxes are check then add the variables to the lower case letters
+  if (abc) {
+    passCode += upperCase;
+  }
+  if (nums) {
+    passCode += numbers;
+  }
+  if (spec) {
+    passCode += specialChar;
+  }
+  var passCodeEl = "";
+  var pLength = parseInt(document.getElementById("passLength").value)
+  for (let i = 0; i < pLength.length; i++) {
+    var randomP = Math.floor(Math.random() * passCode.length);
+    passCodeEl += passCode[randomP];
+    console.log(passCodeEl)
+  }
+  return passCodeEl;
 }
 
-}
-generatePassword()
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+console.log(password)
+    passwordText.value = password;
+  }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
 
